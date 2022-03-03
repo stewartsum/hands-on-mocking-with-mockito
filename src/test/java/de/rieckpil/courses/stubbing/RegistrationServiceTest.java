f21236f3140442cfe43d0d2ecccb34c7f372c3e9
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-03 00:50:14 -0700

    Stubbing Method Calls Of A Mock
    - Second Test For Our Class Under Test

--- src/test/java/de/rieckpil/courses/stubbing/RegistrationServiceTest.java
+++ src/test/java/de/rieckpil/courses/stubbing/RegistrationServiceTest.java
@@ -129,5 +129,17 @@ public class RegistrationServiceTest {
 
   @Test
   void shouldAllowRegistrationOfNewUser() {
+
+    when(bannedUsersClient.isBanned(eq("duke"), any(Address.class))).thenReturn(false);
+    when(userRepository.findByUsername("duke")).thenReturn(null);
+    when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
+      User user = invocation.getArgument(0);
+      user.setId(42L);
+      return user;
+    });
+
+    User user = cut.registerUser("duke", Utils.createContactInformation("duke@mockito.org"));
+
+    assertNotNull(user);
   }
 }
