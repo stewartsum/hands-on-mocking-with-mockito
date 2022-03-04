09baadc7d04648cf88f58fe116208ac4937e2fbb
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-04 02:29:25 -0700

    Miscellaneous & Advanced Topics
    - Deep Stubbing

--- src/test/java/de/rieckpil/courses/advanced/DeepStubTest.java
+++ src/test/java/de/rieckpil/courses/advanced/DeepStubTest.java
@@ -13,9 +13,22 @@ public class DeepStubTest {
 
   @Test
   void withoutDeepStubs() {
+
+    ContactInformation contactInformation = Mockito.mock(ContactInformation.class);
+    Address address = Mockito.mock(Address.class);
+
+    Mockito.when(contactInformation.getAddress()).thenReturn(address);
+    Mockito.when(address.getCity()).thenReturn("Berlin");
+
+    System.out.println(contactInformation.getAddress().getCity()); // Berlin
   }
 
   @Test
   void deepStubs() {
+    ContactInformation contactInformation = Mockito.mock(ContactInformation.class, Answers.RETURNS_DEEP_STUBS);
+
+    Mockito.when(contactInformation.getAddress().getCity()).thenReturn("Berlin");
+
+    System.out.println(contactInformation.getAddress().getCity()); // Berlin
   }
 }
