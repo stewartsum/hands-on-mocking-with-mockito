f810886fb98757dbca657d575f50de3bb2a2c492
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-04 02:59:41 -0700

    Miscellaneous & Advanced Topics
    - How To Mock Constructors?

--- src/test/java/de/rieckpil/courses/advanced/ConstructorMockTest.java
+++ src/test/java/de/rieckpil/courses/advanced/ConstructorMockTest.java
@@ -13,6 +13,30 @@ class ConstructorMockTest {
 
   @Test
   void constructorMocking() {
+
+    System.out.println(new JpaUserRepository().findByUsername("mike"));
+    /*
+    Retrieving a given user from the database
+    null
+     */
+
+
+    try (MockedConstruction<JpaUserRepository> mocked = Mockito.mockConstruction(JpaUserRepository.class)) {
+
+      JpaUserRepository jpaUserRepository = new JpaUserRepository();
+
+      Mockito.when(jpaUserRepository.findByUsername("duke")).thenReturn(new User());
+
+      assertNotNull(jpaUserRepository.findByUsername("duke"));
+
+      Mockito.verify(jpaUserRepository).findByUsername("duke");
+    }
+
+    System.out.println(new JpaUserRepository().findByUsername("duke"));
+    /*
+    Retrieving a given user from the database
+    null
+     */
   }
 
   @Test
