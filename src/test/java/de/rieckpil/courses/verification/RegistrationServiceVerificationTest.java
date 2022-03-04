684cfad2e1fe3f58048e3a2a99bc34ed8a655505
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-04 00:44:05 -0700

    Verifying The Interaction Of A Mock
    - Introduction To Verifications With Mockito

--- src/test/java/de/rieckpil/courses/verification/RegistrationServiceVerificationTest.java
+++ src/test/java/de/rieckpil/courses/verification/RegistrationServiceVerificationTest.java
@@ -4,6 +4,7 @@ import de.rieckpil.courses.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
+import org.mockito.exceptions.base.MockitoAssertionError;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
@@ -36,6 +37,28 @@ public class RegistrationServiceVerificationTest {
 
   @Test
   void basicVerification() {
+
+    when(bannedUsersClient.isBanned(eq("duke"), any(Address.class))).thenReturn(true);
+    // when(bannedUsersClient.amountOfGloballyBannedAccounts()).thenReturn(42L);
+    /*
+
+    [MockitoHint] de.rieckpil.courses.verification.RegistrationServiceVerificationTest (see javadoc for MockitoHint):
+    [MockitoHint] 1. Unused -> at de.rieckpil.courses.verification.RegistrationServiceVerificationTest.basicVerification(RegistrationServiceVerificationTest.java:41)
+
+     */
+
+    assertThrows(IllegalArgumentException.class,
+      () -> cut.registerUser("duke", Utils.createContactInformation("duke@mockito.org")));
+
+    Mockito.verify(bannedUsersClient).isBanned(eq("duke"), argThat(address -> address.getCity().equals("Berlin")));
+    Mockito.verify(bannedUsersClient, times(1)).isBanned(eq("duke"), any(Address.class));
+    Mockito.verify(bannedUsersClient, atLeastOnce()).isBanned(eq("duke"), any(Address.class));
+    Mockito.verify(bannedUsersClient, atMost(1)).isBanned(eq("duke"), any(Address.class));
+    Mockito.verify(bannedUsersClient, never()).bannedUserId();
+
+    Mockito.verifyNoMoreInteractions(bannedUsersClient, userRepository);
+
+    assertThrows(MockitoAssertionError.class, () -> Mockito.verify(bannedUsersClient, description("Nobody checked for mike")).isBanned(eq("mike"), any(Address.class)));
   }
 
   @Test
