ff8ef29c638225bae0f79f657badd0cb75d6e237
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-04 02:19:08 -0700

    Miscellaneous & Advanced Topics
    - @Mock vs. @Spy

--- src/test/java/de/rieckpil/courses/advanced/SpyTest.java
+++ src/test/java/de/rieckpil/courses/advanced/SpyTest.java
@@ -3,6 +3,7 @@ package de.rieckpil.courses.advanced;
 import de.rieckpil.courses.BannedUsersClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,9 +19,24 @@ class SpyTest {
 
   @Test
   void spies() {
+
+    Mockito.when(bannedUsersClient.amountOfBannedAccounts()).thenReturn(500);
+
+    System.out.println(bannedUsersClient.banRate()); // 42.42
+    System.out.println(bannedUsersClient.amountOfBannedAccounts()); // 500
+    System.out.println(bannedUsersClient.bannedUserId()); // [1, 2, 3]
   }
 
   @Test
   void spiesGotcha() {
+
+    List spiedList = Mockito.spy(ArrayList.class);
+
+    // Mockito.when(spiedList.get(0)).thenReturn("spy"); // java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0
+
+    Mockito.doReturn("spy").when(spiedList).get(0);
+
+    System.out.println(spiedList.get(0)); // spy
+    System.out.println(spiedList.get(1)); // java.lang.IndexOutOfBoundsException: Index 1 out of bounds for length 0
   }
 }
